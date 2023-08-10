b88a044d025a64cd33a28f147715c6d854be0659
Les Chinyanga <les.chinyanga@africacode.academy>
2023-08-10 13:29:17 +0200

    Add information about status, adding and commiting

--- app.js
+++ app.js
@@ -1,3 +1,8 @@
+// git status - check the status of the repo
+// git add <filename> - add file to the staging area
+// git commit -m "<message>" - add commit message
+// "the purpose of this commit is to ...."
+
 console.log("init");
 
 console.log("hello");
