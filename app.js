3d60da15d074f884352408bfbdb050ada669ffa7
Les Chinyanga <les.chinyanga@africacode.academy>
2023-08-10 13:43:45 +0200

    Add information about git log

--- app.js
+++ app.js
@@ -4,6 +4,8 @@
 // git commit -m "<message>" - add commit message
 // "the purpose of this commit is to ...."
 // git commit --amend -m "<message>" - change the commit message
+// git log - show the commit history
+// git log --oneline - show the commit history in one line
 
 console.log("init");
 
